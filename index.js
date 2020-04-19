ef1215f2e9740fb50366b0cdc786538e4c89c46b
Max McKenzie <max@seam.es>
2020-04-19 13:14:28 +0200

    added index that exports atjs modules

---
+++ index.js
@@ -0,0 +1,6 @@
+exports.build = require('./build');
+exports.dev = require('./dev');
+exports.i18n = require('./i18n');
+exports.server = require('./express');
+exports.osiris = require('./osiris');
+exports.osirisCMS = require('./osiris-cms');
